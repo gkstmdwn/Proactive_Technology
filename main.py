cf85be047124b57933abcd5d34377626c5873075
SeungJoo Han <sugarcanes0118@gmail.com>
2023-06-15 09:53:34 +0900

    dfd

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import csv
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QTextEdit
 import Study
 
 class STUDY():
@@ -34,26 +34,25 @@ class STUDY():
         target_student_grade = self.student_dict[target_student].grade[subject]
         mentor_list = []
         for student in self.student_dict.values():
-            if student.grade[subject] > target_student_grade + 10:
+            if student.grade[subject] > target_student_grade + self.grade_stdev[subject]:
                 mentor_list.append(student)
-        return mentor_list
+        return str(mentor_list)
 
+    def recommend_mentee(self, target_student:str, subject) -> list:
+        target_student_grade = self.student_dict[target_student].grade[subject]
+        mentor_list = []
+        for student in self.student_dict.values():
+            if student.grade[subject] < target_student_grade - self.grade_stdev[subject]:
+                mentor_list.append(student)
+        return str(mentor_list)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def recommend_study_group(self, target_student:str, subject) -> list:
+        target_student_grade = self.student_dict[target_student].grade[subject]
+        mentor_list = []
+        for student in self.student_dict.values():
+            if student.grade[subject] > target_student_grade - self.grade_stdev[subject] and student.grade[subject] < target_student_grade + self.grade_stdev[subject]:
+                mentor_list.append(student)
+        return str(mentor_list)
 
 class MyApp(QWidget):
     def __init__(self):
@@ -63,41 +62,68 @@ class MyApp(QWidget):
 
     def initUI(self):
         # GUI의 설정을 합니다.
-        self.setWindowTitle('Mentor Recommender')
+        self.setWindowTitle('Recommender')
 
         # Vertical layout을 설정합니다.
         layout = QVBoxLayout()
 
-        # 학생 이름을 입력받을 위젯을 생성합니다.
-        self.student_input = QLineEdit()
-        self.student_input.setPlaceholderText("Enter Student's Name")
+        # 이름을 입력받을 위젯을 생성합니다.
+        self.name_input = QLineEdit()
+        self.name_input.setPlaceholderText("Enter Name")
 
         # 과목명을 입력받을 위젯을 생성합니다.
         self.subject_input = QLineEdit()
         self.subject_input.setPlaceholderText("Enter Subject")
 
         # Button을 생성하고 누르면 recommend_mentor 함수의 결과를 라벨에 보여줍니다.
-        self.button = QPushButton('Recommend a Mentor', self)
-        self.button.clicked.connect(self.on_click)
+        self.mentor_button = QPushButton('Recommend a Mentor', self)
+        self.mentor_button.clicked.connect(self.on_mentor_click)
+
+        # Button을 생성하고 누르면 recommend_mentee 함수의 결과를 라벨에 보여줍니다.
+        self.mentee_button = QPushButton('Recommend a Mentee', self)
+        self.mentee_button.clicked.connect(self.on_mentee_click)
+
+        # Button을 생성하고 누르면 recommend_study_group 함수의 결과를 라벨에 보여줍니다.
+        self.study_group_button = QPushButton('Recommend a Study Group', self)
+        self.study_group_button.clicked.connect(self.on_study_group_click)
 
         # 결과를 보여줄 라벨을 생성합니다.
-        self.label = QLabel()
+        self.text_edit = QTextEdit()
+        self.text_edit.setReadOnly(True)
 
         # 레이아웃에 위젯을 추가합니다.
-        layout.addWidget(self.student_input)
+        layout.addWidget(self.name_input)
         layout.addWidget(self.subject_input)
-        layout.addWidget(self.button)
-        layout.addWidget(self.label)
+        layout.addWidget(self.mentor_button)
+        layout.addWidget(self.mentee_button)
+        layout.addWidget(self.study_group_button)
+        layout.addWidget(self.text_edit)
 
         self.setLayout(layout)
 
-    def on_click(self):
+    def on_mentor_click(self):
         # 버튼을 누르면 recommend_mentor 함수의 결과를 라벨에 보여줍니다.
-        student_name = self.student_input.text()
+        name = self.name_input.text()
         subject = self.subject_input.text()
 
-        recommendation = self.Study_Program.recommend_mentor(student_name, subject)
-        self.label.setText(str(recommendation))
+        recommendation = self.Study_Program.recommend_mentor(name, subject)
+        self.text_edit.setText(recommendation)
+
+    def on_mentee_click(self):
+        # 버튼을 누르면 recommend_mentee 함수의 결과를 라벨에 보여줍니다.
+        name = self.name_input.text()
+        subject = self.subject_input.text()
+
+        recommendation = self.Study_Program.recommend_mentee(name, subject)
+        self.text_edit.setText(recommendation)
+
+    def on_study_group_click(self):
+        # 버튼을 누르면 recommend_study_group 함수의 결과를 라벨에 보여줍니다.
+        name = self.name_input.text()
+        subject = self.subject_input.text()
+
+        recommendation = self.Study_Program.recommend_study_group(name, subject)
+        self.text_edit.setText(recommendation)
 
 
 def main():
